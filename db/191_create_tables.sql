6bc5fd642b59ee5d234cf02ee6687d1b30c856ac
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-08-06 08:10:07 +0000

    Init model

---
+++ db/191_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 191, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table songs.
+--
+CREATE TABLE caeschema.songs (
+  title VARCHAR(255) ,
+CONSTRAINT title_PK PRIMARY KEY (title)
+ 
+);
+
+
+
